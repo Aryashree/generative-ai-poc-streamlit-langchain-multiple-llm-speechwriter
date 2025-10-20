49ceffd9e66a8b1615c93ff0f0139335f22f9d1b
Aryashree <aryasree.mohapatra@gmail.com>
2025-10-20 13:42:18 -0700

    Add files via upload

---
+++ multi-llm-chain.py
@@ -0,0 +1,47 @@
+import os
+from langchain_openai import ChatOpenAI
+from langchain_community.chat_models import ChatOllama
+import streamlit as st
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser, JsonOutputParser
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+llm1 = ChatOpenAI(model="gpt-4o", api_key=OPENAI_API_KEY)
+llm2 = ChatOllama(model="gemma:2b")
+
+title_prompt = PromptTemplate(
+    input_variables=["topic"],
+    template="""You are an experienced speech writer.
+    You need to craft an impactful title for a speech 
+    on the following topic: {topic}
+    Return your response as JSON with the key "title".
+    """
+)
+
+speech_prompt = PromptTemplate(
+    input_variables=["title"],
+    template="""You need to write a powerful speech of 350 words
+    for the following title: {title}
+    Return your response as JSON with the key "speech".
+    """
+)
+
+first_chain = title_prompt | llm1 | JsonOutputParser() | (lambda result: (st.write(result["title"]), result["title"])[1])
+second_chain = speech_prompt | llm2 | JsonOutputParser()
+##
+#You can also do this instead of lambda
+#def display_and_return_title(title):
+#    st.write(title)  # Show it
+#    return title     # Pass it along
+
+# Instead of: (lambda title: (st.write(title), title)[1])
+# Use: display_and_return_title
+##
+final_chain = first_chain | (lambda title:{"title":title}) | second_chain
+
+st.title("Speech Generator")
+topic = st.text_input("Enter the topic:")
+
+if topic:
+    response = final_chain.invoke({"topic": topic})
+    st.write(response)
