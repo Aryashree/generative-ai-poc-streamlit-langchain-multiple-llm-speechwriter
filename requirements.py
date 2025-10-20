d6dcaedac9bac893cc9436cc0ebbaa8707a81512
Aryashree <aryasree.mohapatra@gmail.com>
2025-10-20 13:54:44 -0700

    Update requirements.py

--- requirements.py
+++ requirements.py
@@ -1,4 +1,8 @@
-streamlit
-langchain-openai
-langchain
-langchain-ollama
+streamlit>=1.36.0
+langchain>=0.2.0
+langchain-core>=0.2.0
+langchain-community>=0.2.0
+langchain-openai>=0.2.0
+ollama>=0.1.7
+openai>=1.35.0
+tiktoken
